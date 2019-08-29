389cc8fc6e0e36b25dd6681ea2a307cfd40af357
Bruno Souza <brunoprogramadorjava@gmail.com>
2019-08-29 15:06:04 -0300

    [t01] Initial commit Smart Point API with Spring and Java 8

---
+++ src/main/java/com/brunosouza/smartpoint/api/PointSmartApplication.java
@@ -0,0 +1,13 @@
+package com.brunosouza.smartpoint.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointSmartApplication.class, args);
+	}
+
+}
